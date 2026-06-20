43cc74437ddd22b79d56fee056b69fdcdc358d9b
k4rt_c0b <mridul25sharma52007@gmail.com>
2026-06-20 16:11:15 +0530

    fix(auth): update CLIENT_ID in .env and enhance verification logic in verify.js

--- aws/lambda/usermanagement/auth/verify.js
+++ aws/lambda/usermanagement/auth/verify.js
@@ -1,9 +1,43 @@
-exports.verify = async (event) => {
-  const body = JSON.parse(event.body);
-  const { code } = body;
+const cognito = require("../utils/cognito");
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ verified: true }),
-  };
+const {
+  ConfirmSignUpCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
+
+exports.verify = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+
+    const { email, code } = body;
+
+    if (!email || !code) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "OTP required",
+        }),
+      };
+    }
+
+    const command = new ConfirmSignUpCommand({
+      ClientId: process.env.CLIENT_ID,
+      Username: email,
+      ConfirmationCode: code,
+    });
+
+    await cognito.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Account Verified",
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
 };
