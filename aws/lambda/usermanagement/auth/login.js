f6612629a24fe6bf23f4b2844d1044ca5a109817
k4rt_c0b <mridul25sharma52007@gmail.com>
2026-06-20 16:16:38 +0530

    feat(auth): implement login functionality with Cognito authentication and return access tokens

--- aws/lambda/usermanagement/auth/login.js
+++ aws/lambda/usermanagement/auth/login.js
@@ -1,9 +1,38 @@
-exports.login = async (event) => {
-  const body = JSON.parse(event.body);
-  const { email, password } = body;
+const cognito = require("../utils/cognito");
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ email }),
-  };
+const {
+  InitiateAuthCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
+
+exports.login = async (event) => {
+  try {
+    const { email, password } = JSON.parse(event.body);
+
+    const command = new InitiateAuthCommand({
+      AuthFlow: "USER_PASSWORD_AUTH",
+      ClientId: process.env.CLIENT_ID,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
+    });
+
+    const result = await cognito.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        accessToken: result.AuthenticationResult.AccessToken,
+        idToken: result.AuthenticationResult.IdToken,
+        refreshToken: result.AuthenticationResult.RefreshToken,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
 };
