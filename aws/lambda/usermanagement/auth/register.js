a23a8a148251b559dcd3d9322beebd8a5c7ccc78
k4rt_c0b <mridul25sharma52007@gmail.com>
2026-06-20 15:28:43 +0530

    feat(auth): enhance user registration with error handling and add environment configuration

--- aws/lambda/usermanagement/auth/register.js
+++ aws/lambda/usermanagement/auth/register.js
@@ -1,13 +1,42 @@
+const cognito = require("../utils/cognito");
+
+const { SignUpCommand } = require("@aws-sdk/client-cognito-identity-provider");
+
 exports.register = async (event) => {
-  const body = JSON.parse(event.body);
+  try {
+    const body = JSON.parse(event.body);
+    const { email, password } = body;
+    if (!email || !password) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Email and Password is required",
+        }),
+      };
+    }
 
-  const { email, password } = body;
+    const command = new SignUpCommand({
+      ClientId: process.env.ClientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [{ Name: "email", Value: email }],
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  };
+    const results = await cognito.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Verfication Code Sent",
+        userSub: results.UserSub,
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
 };
