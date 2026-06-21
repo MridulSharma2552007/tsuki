0c5be9be2d16fdb1e904ec518bdeb15d6d64cff2
k4rt_c0b <mridul25sharma52007@gmail.com>
2026-06-21 12:06:43 +0530

    feat(auth): refactor loginwidget to StatefulWidget, enhance login functionality with validation and error handling

--- lib/app/feature/auth/presentation/pages/login_page.dart
+++ lib/app/feature/auth/presentation/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tsuki/app/feature/auth/bloc/auth_bloc.dart';
+import 'package:tsuki/app/feature/auth/bloc/auth_event.dart';
 import 'package:tsuki/app/feature/auth/bloc/auth_state.dart';
 import 'package:tsuki/core/constants/ascii.dart';
 import 'package:tsuki/core/theme/app_text_style.dart';
@@ -56,10 +57,52 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class loginwidget extends StatelessWidget {
+class loginwidget extends StatefulWidget {
   final PageController _controller;
   const loginwidget({super.key, required PageController controller})
     : _controller = controller;
+
+  @override
+  State<loginwidget> createState() => _loginwidgetState();
+}
+
+class _loginwidgetState extends State<loginwidget> {
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
+  bool isValidPassword(String password) {
+    final regex = RegExp(
+      r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$',
+    );
+
+    return regex.hasMatch(password);
+  }
+
+  void login() {
+    final email = emailcontroller.text.trim();
+    final password = passwordcontroller.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Email and password required')),
+      );
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Password must contain uppercase, lowercase, number and symbol',
+          ),
+        ),
+      );
+      return;
+    }
+
+    context.read<AuthBloc>().add(
+      LoginRequested(email: email, password: password),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,15 +131,21 @@ class loginwidget extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 40),
-              CustomTextField(obj: 'EMAIL'),
+              CustomTextField(
+                obj: 'EMAIL',
+                textEditingController: emailcontroller,
+              ),
               SizedBox(height: 40),
-              CustomTextField(obj: 'PASSWORD'),
+              CustomTextField(
+                obj: 'PASSWORD',
+                textEditingController: passwordcontroller,
+              ),
               SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   GestureDetector(
-                    onTap: () => _controller.animateToPage(
+                    onTap: () => widget._controller.animateToPage(
                       1,
                       duration: const Duration(milliseconds: 500),
                       curve: Curves.easeInOut,
@@ -129,7 +178,7 @@ class loginwidget extends StatelessWidget {
                       return const Color(0xFFFFB000);
                     }),
                   ),
-                  onPressed: () {},
+                  onPressed: () => login(),
                   child: Text(
                     " > LOGIN",
                     style: TextStyle(
@@ -149,11 +198,17 @@ class loginwidget extends StatelessWidget {
 
 class CustomTextField extends StatelessWidget {
   final String obj;
-  const CustomTextField({super.key, required this.obj});
+  final TextEditingController textEditingController;
+  const CustomTextField({
+    super.key,
+    required this.obj,
+    required this.textEditingController,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: textEditingController,
       style: const TextStyle(
         color: Color(0xFFFFB000),
         fontFamily: 'Courier',
