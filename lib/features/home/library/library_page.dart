a44347f8554749199b2930939dbbf7293b637b32
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-07-11 15:42:35 +0530

    added basic library structure

--- lib/features/home/library/library_page.dart
+++ lib/features/home/library/library_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:tsuki/app/theme/app_text_theme.dart';
 
 class LibraryPage extends StatelessWidget {
   const LibraryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screen_height = MediaQuery.of(context).size.height;
+    final screen_width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: screen_height * 0.1),
+          Text("Your library", style: AppTextTheme.screenTitleLarge),
+          Text(
+            "Everything you've saved, all in one place",
+            style: AppTextTheme.secondary,
+          ),
+        ],
+      ),
+    );
   }
 }
